b1e0625fb6bb1f3adfd04167ad1d8b56c47d82e5
IvanFlores387 <145875422+IvanFlores387@users.noreply.github.com>
2026-05-15 18:00:52 -0600

    correccion

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'medico/registro-consulta/:appointmentId',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'medico/historial-paciente/:patientId',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'admin/historial-paciente/:patientId',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
